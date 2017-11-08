1a694c75cdb4d5f0533bb504c54635c4613f335b
Nick Eddy <nick.a.eddy@gmail.com>
2017-11-08 13:02:42 -0800

    played around with project cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,12 +4,14 @@ import "./Project.css";
 class Project extends Component {
   render() {
     return (
-        <div className="project">
+        <div className="project__card">
           <img className="project__image" src={this.props.imagePath} />
-          <p>This is a short blurb of text about this project.</p>
-          <div className="project__icons">
-            <a alt="a project" href="#"><i className="fa fa-external-link fa-2x"></i></a>
-            <a alt="a project" href="#"><i className="fa fa-github fa-2x"></i></a>
+          <div className="project__info">
+            <p>This is a short blurb of text about this project.</p>
+            <div className="project__icons">
+              <a alt="a project" href="#"><i className="fa fa-external-link fa-2x"></i></a>
+              <a alt="a project" href="#"><i className="fa fa-github fa-2x"></i></a>
+            </div>
           </div>
         </div>
     );
