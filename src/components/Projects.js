a36da7912e0688cfbef472387e22f04d24eb04a7
Nick Eddy <nick.a.eddy@gmail.com>
2017-11-20 15:54:55 -0800

    added pokemon type quiz project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,7 +10,7 @@ class Projects extends Component {
             <div className = "projectWrap">
               <Project imagePath="images/mastermindScreenShot.png" projectName="Mastermind"/>
               <Project imagePath="images/washcycleScreenShot.png" projectName="Washcycle"/>
-              <Project imagePath="http://placeimg.com/1000/500/tech?t=1509152053103" projectName="Test"/>
+              <Project imagePath="images/pokemonScreenShot.png" projectName="Pokemon Type Quiz"/>
               <Project imagePath="http://placeimg.com/1000/500/tech?t=1509152053103" projectName="Test"/>
             </div>
         </div>
