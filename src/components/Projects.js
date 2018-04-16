1035da1a3be4878d90552f026bfea3d524826242
Nick Eddy <nick.a.eddy@gmail.com>
2018-04-15 17:27:25 -0700

    added weather app

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,6 +8,12 @@ class Projects extends Component {
         <div className = "section-projects" id = "projects">
             <h2 className = "heading">Projects</h2>
             <div className = "projectWrap">
+              <Project imagePath="images/weatherScreenShot.png" 
+                       projectName="React Weather App" 
+                       githubLink="https://github.com/OctoNerd/react-weather"
+                       liveLink="https://octonerd.github.io/react-weather" 
+                       projectDescription="Weather app built with react.js using Open Weather Maps API"
+                       technologiesUsed="React, owm API, sass, webpack, node"/>
               <Project imagePath="images/mastermindScreenShot.png" 
                        projectName="Mastermind" 
                        githubLink="https://github.com/OctoNerd/mastermind" 
@@ -15,22 +21,16 @@ class Projects extends Component {
                        projectDescription="Recreation of the 1970 boardgame of the same name. Built with Javascript."
                        technologiesUsed="Javascript"/>
               <Project imagePath="images/washcycleScreenShot.png" 
-                       projectName="Washcycle" 
+                       projectName="Washcycle company site" 
                        githubLink="https://github.com/OctoNerd/washcycle" 
                        liveLink="http://www.washcyclewindows.com" 
-                       projectDescription="Web presence for a window cleaning company."
-                       technologiesUsed="Javascript"/>
+                       projectDescription="Web presence for a Portland window cleaning company."
+                       technologiesUsed="HTML, CSS"/>
               <Project imagePath="images/pokemonScreenShot.png" 
                        projectName="Pokemon Type Quiz" 
                        githubLink="https://github.com/OctoNerd/pokemonTypeQuiz" 
                        liveLink="https://octonerd.github.io/pokemonTypeQuiz" 
                        projectDescription="Quiz game built with Javascript and css."
-                       technologiesUsed="Javascript"/>
-              <Project imagePath="http://placeimg.com/1000/500/tech?t=1509152053103" 
-                       projectName="Test" 
-                       githubLink="#" 
-                       liveLink="#" 
-                       projectDescription="test"
                        technologiesUsed="Javascript"/>
             </div>
         </div>
